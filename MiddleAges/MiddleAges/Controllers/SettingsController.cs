29c3a230c41e64fcdf65fc8433c93b5a0f6c3bbb
Lezhik <greennos16@gmail.com>
2024-02-01 14:43:54 +0300

    feat: new window settings

---
+++ MiddleAges/MiddleAges/Controllers/SettingsController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MiddleAges.Data;
+using MiddleAges.Entities;
+using MiddleAges.Models;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiddleAges.Controllers
+{
+    public class SettingsController : Controller
+    {
+        private readonly ILogger<MainController> _logger;
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public SettingsController(ILogger<MainController> logger, ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _logger = logger;
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            var user = _userManager.GetUserAsync(HttpContext.User).Result;
+
+            Player player = _context.Players.FirstOrDefault(k => k.PlayerId.ToString() == user.Id);
+
+            return View("Settings", player);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+    }
+}
