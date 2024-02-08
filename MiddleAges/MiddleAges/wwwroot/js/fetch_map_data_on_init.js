fcfcf7046b31aab547d3bf09caa4319907953b64
Kirill-Spesivtsev <ksby8819@gmail.com>
2024-02-08 22:25:32 +0300

    feat: add filling of region colours; fix some regions display

---
+++ MiddleAges/MiddleAges/wwwroot/js/fetch_map_data_on_init.js
@@ -0,0 +1,31 @@
+﻿$(document).load(colorizeMapLands());
+
+function colorizeMapLands() {
+    $.ajax({
+        url: 'Map/FetchLandColors',
+        type: 'get',
+        datatype: 'json',
+        contentType: 'application/json;charset=utf-8',
+        cache: false,
+        success: function (response) {
+            if (response == null || response == undefined || response.length == 0) {
+                console.log("Error loading map colors");
+            }
+            else {
+                return response;
+            }
+        },
+        error: function (response) {
+            console.log("Error loading map colors");
+        }
+    })
+    .done(function (data) {
+        if (data != null) {
+            data.forEach(
+                function (elem) {
+                    $('#' + elem.landId.replace(' ', '_')).css("fill", elem.color);
+                }
+            );
+        }
+    })
+}
