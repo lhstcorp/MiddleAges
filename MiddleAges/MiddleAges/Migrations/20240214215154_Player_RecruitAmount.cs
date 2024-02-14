8a4851d9d8a242c97dfc7dacc539ead06c815a7c
Smirnoff <68186730+lhstcorp@users.noreply.github.com>
2024-02-15 00:57:25 +0300

    feat: made ajax function of updating avatar; add recruit amount field to player;

---
+++ MiddleAges/MiddleAges/Migrations/20240214215154_Player_RecruitAmount.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MiddleAges.Migrations
+{
+    public partial class Player_RecruitAmount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RecruitAmount",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RecruitAmount",
+                table: "AspNetUsers");
+        }
+    }
+}
---
+++ MiddleAges/MiddleAges/wwwroot/js/update_avatar_img_by_click.js
@@ -0,0 +1,33 @@
+﻿$('#update_avatar_btn').click(function (event) {
+    let url = $('#avatar_selected_img').attr('src');
+
+    updateAvatar(url);
+});
+
+function updateAvatar(url) {
+    $.ajax({
+        type: 'POST',
+        dataType: 'JSON',
+        url: 'Settings/UpdateAvatar',
+        data: { selectedImageId: url },
+        success: function (response) {
+            if (response == null || response == undefined || response.length == 0) {
+                return 'Error';
+            }
+            else {
+                return response;
+            }
+        },
+        error: function (response) {
+            return 'Error';
+        }
+    })
+    .done(function (data) {
+        if (data == 'OK') {
+            $('#player_avatar').attr('src', url);
+        }
+    })
+    .fail(function (data) {
+        // ToDo error log.
+    });
+}
