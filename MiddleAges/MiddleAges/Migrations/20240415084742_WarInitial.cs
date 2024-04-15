a3f94dd72f77d9d3b78df97e29b3ab3986476446
Smirnoff <68186730+lhstcorp@users.noreply.github.com>
2024-04-15 11:58:40 +0300

    feat: added wars and war declarations;

---
+++ MiddleAges/MiddleAges/Migrations/20240415084742_WarInitial.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MiddleAges.Migrations
+{
+    public partial class WarInitial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Wars",
+                columns: table => new
+                {
+                    WarId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    LandIdFrom = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LandIdTo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StartDateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsEnded = table.Column<bool>(type: "bit", nullable: false),
+                    WarResult = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Wars", x => x.WarId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Wars");
+        }
+    }
+}
