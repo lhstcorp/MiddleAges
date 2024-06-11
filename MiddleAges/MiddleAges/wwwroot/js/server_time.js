7e286b79547450f9bbf2a010e47bbe79f24cfbba
Lezhik <greennos16@gmail.com>
2024-06-11 17:44:29 +0300

    feat: server time, dismiss units, balance money

---
+++ MiddleAges/MiddleAges/wwwroot/js/server_time.js
@@ -0,0 +1,5 @@
+﻿var time = setInterval(function () {
+    var date = new Date();
+    document.getElementById("serverTime").innerHTML = (date.getUTCDate() + "." + (date.getUTCMonth() + 1) + "." + date.getUTCFullYear() + " " + date.getUTCHours(0) + ":" + date.getUTCMinutes() + ":" + date.getUTCSeconds());
+},
+1000);
