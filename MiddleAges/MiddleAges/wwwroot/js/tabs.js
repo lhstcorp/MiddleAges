179f0c74ee07ac7145d529c2688c62e1990fd582
Smirnoff <68186730+lhstcorp@users.noreply.github.com>
2024-06-24 18:00:16 +0300

    feat: rating system; design: rating

--- MiddleAges/MiddleAges/wwwroot/js/tabs.js
+++ MiddleAges/MiddleAges/wwwroot/js/tabs.js
@@ -1,20 +1,20 @@
-function rating(evt, attribute) {
-    // Объявить все переменные
-    var i, tabcontent, tablinks;
-    // Получить все элементы с помощью class="tabcontent" и спрятать их
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    // Получить все элементы с помощью class="tablinks" и удалить class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    // Показать текущую вкладку и добавить "active" класс для кнопки, которая открыла вкладку
-    document.getElementById(attribute).style.display = "block";
-    evt.currentTarget.className += " active";
-}
+//function rating(evt, attribute) {
+//    // Объявить все переменные
+//    var i, tabcontent, tablinks;
+//    // Получить все элементы с помощью class="tabcontent" и спрятать их
+//    tabcontent = document.getElementsByClassName("tabcontent");
+//    for (i = 0; i < tabcontent.length; i++) {
+//        tabcontent[i].style.display = "none";
+//    }
+//    // Получить все элементы с помощью class="tablinks" и удалить class "active"
+//    tablinks = document.getElementsByClassName("tablinks");
+//    for (i = 0; i < tablinks.length; i++) {
+//        tablinks[i].className = tablinks[i].className.replace(" active", "");
+//    }
+//    // Показать текущую вкладку и добавить "active" класс для кнопки, которая открыла вкладку
+//    document.getElementById(attribute).style.display = "block";
+//    evt.currentTarget.className += " active";
+//}
 
 function wars(evt, attribute) {
     // Объявить все переменные
@@ -31,5 +31,5 @@ function wars(evt, attribute) {
     }
     // Показать текущую вкладку и добавить "active" класс для кнопки, которая открыла вкладку
     document.getElementById(attribute).style.display = "block";
-    evt.currentTarget.className += " active";
+    evt.target.className += " active";
 }
