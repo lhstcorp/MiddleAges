7a3fce38b36f95fecd4d2b4f40703584354e0053
Smirnoff <68186730+lhstcorp@users.noreply.github.com>
2024-07-02 17:28:36 +0300

    feat: modal player dialog

---
+++ MiddleAges/MiddleAges/ViewModels/ModalPlayerViewModel.cs
@@ -0,0 +1,19 @@
+﻿using MiddleAges.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiddleAges.ViewModels
+{
+    public class ModalPlayerViewModel
+    {
+        public Player Player { get; set; }
+        public Rating Rating { get; set; }
+        public PlayerInformation PlayerInformation { get; set; }
+        public Land ResidenceLand { get; set; }
+        public Country ResidenceCountry { get; set; }
+        public Unit Peasants { get; set; }
+
+    }
+}
---
+++ MiddleAges/MiddleAges/wwwroot/js/modal_player.js
@@ -0,0 +1,52 @@
+﻿$(document).ready(function () {
+    $(document).on("click", ".m_playerBtn", showModalPlayerDialog);
+});
+
+function showModalPlayerDialog() {
+    var playerId = $(this).data("playerid");
+
+    if (playerId.length > 0) {
+        getPlayerById(playerId);
+    }    
+
+    $('#m_player_dialog').modal('show');
+}
+function hideModalPlayerDialog() {
+    $('#m_player_dialog').modal('hide');
+}
+
+function getPlayerById(id) {
+    $.ajax({
+        url: 'Main/GetPlayerById/' + id,
+        type: 'get',
+        datatype: 'json',
+        contentType: 'application/json;charset=utf-8',
+        success: function (response) {
+            if (response == null || response == undefined || response.length == 0) {
+                return 'NotFound';
+            }
+            else {
+                return response;
+            }
+        },
+        error: function (response) {
+            return 'NotFound';
+        }
+    })
+        .done(function (data) {
+            if (data != 'NotFound') {
+                let obj = JSON.parse(data);
+                populateModalPlayerDialog(obj);
+            }
+            else {
+                alert("Failed to load player data");
+            }
+        })
+        .fail(function (data) {
+            alert("Failed to load player data");
+        });
+}
+
+function populateModalPlayerDialog(obj) {
+    $('#m_player_playerName').text(obj.Player.UserName);
+}
