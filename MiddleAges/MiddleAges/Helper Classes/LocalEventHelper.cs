3561664629e9f0683782392d791fc1c3cfa30d14
Lezhik <greennos16@gmail.com>
2024-07-17 17:39:12 +0300

    Feat: 54 local events added

--- MiddleAges/MiddleAges/Helper Classes/LocalEventHelper.cs
+++ MiddleAges/MiddleAges/Helper Classes/LocalEventHelper.cs
@@ -396,6 +396,271 @@ namespace MiddleAges.HelperClasses
              new[] { 0.00, 0.00, 0.00, 0.00, 0.00 },
              new[] { 0.00, 0.00, 0.00, 0.00, 0.00 }));
 
+
+            localEvents.Add(new LocalEvent(
+             33,
+             "Right tributary of the Berezina",
+             "Every year our rivers carry less and less water. If this isn't fixed, there will be a drought next year.",
+             1,
+             "Build an irrigation system",
+             new[] { -2.00, 2.00, 0.20, 0.00, 0.00 },
+             new[] { 100.00, 100.00, 100.00, 0.00, 0.00 },
+             "Import water",
+             new[] { -2.00, 0.00, 0.00, 2.00, 0.00 },
+             new[] { 100.00, 0.00, 0.00, 100.00, 0.00 }));
+
+            localEvents.Add(new LocalEvent(
+             34,
+             "Learning is light and ignorance is darkness",
+             "According to the latest data, our state lacks smart workers. I propose to build a school for our peasants",
+             1,
+             "Build a school",
+             new[] { -1.00, 2.00, 0.50, 0.00, 0.00 },
+             new[] { 100.00, 100.00, 100.00, 0.00, 0.00 },
+             "Build additional barracks",
+             new[] { -1.00, 0.00, 0.00, 0.00, 1.00 },
+             new[] { 100.00, 0.00, 0.00, 0.00, 100.00 }));
+
+            localEvents.Add(new LocalEvent(
+            35,
+            "Don't be a beast!",
+            "People gathered in the square. They protest and are dissatisfied with their living and working conditions. They say that they are tired of enduring",
+            1,
+            "Improve living conditions",
+            new[] { -2.00, 5.00, 0.00, 10.00, 0.00 },
+            new[] { 100.00, 100.00, 0.00, 100.00, 0.00 },
+            "Suppress protests",
+            new[] { 0.00, -1.00, 0.10, -10.00, 1.00 },
+            new[] { 0.00, 100.00, 100.00, 100.00, 100.00 }));
+
+            localEvents.Add(new LocalEvent(
+            36,
+            "Traveling merchant",
+            "A new man has appeared in town and is selling foreign things. He's kind of suspicious",
+            1,
+            "Let him trade",
+            new[] { 2.00, 0.00, 0.00, 0.00, 0.00 },
+            new[] { 100.00, 0.00, 0.00, 0.00, 0.00 },
+            "Drive him out of town",
+            new[] { 0.00, 0.00, 0.10, 0.00, 0.00 },
+            new[] { 0.00, 0.00, 100.00, 0.00, 0.00 }));
+
+            localEvents.Add(new LocalEvent(
+            37,
+            "Is this the holy grail?",
+            "During excavations, your people discovered an ancient artifact from the beginning of our era. What do you want to do with him?",
+            1,
+            "Explore and leave in the museum",
+            new[] { 0.00, 1.00, 0.20, 0.00, 0.00 },
+            new[] { 0.00, 50.00, 100.00, 0.00, 0.00 },
+            "Sell it",
+            new[] { 5.00, 0.00, 0.00, 0.00, 0.00 },
+            new[] { 50.00, 0.00, 0.00, 0.00, 0.00 }));
+
+            localEvents.Add(new LocalEvent(
+            38,
+            "Like a bolt from the blue",
+            "From the letter: my dear, my life is coming to an end. If you can, visit the old man one last time. I'll leave you a small gift. What I have managed to accumulate in my life.",
+            1,
+            "Go for inheritance",
+            new[] { 5.00, 0.00, 0.00, 0.00, 0.00 },
+            new[] { 100.00, 0.00, 0.00, 0.00, 0.00 },
+            "Give it to your servants",
+            new[] { 0.00, 5.00, 0.00, 0.00, 0.00 },
+            new[] { 0.00, 100.00, 0.00, 0.00, 0.00 }));
+
+            localEvents.Add(new LocalEvent(
+            39,
+            "Deserters",
+            "People came to our palace and asked for help and shelter. They are deserters and do not want to fight against their people, but are ready to become our soldiers.",
+            1,
+            "Settle them in the city",
+            new[] { 0.00, 10.00, 0.00, 0.00, 0.00 },
+            new[] { 0.00, 100.00, 0.00, 0.00, 0.00 },
+            "Drive deserters out of the city",
+            new[] { 0.50, 0.00, 0.10, 0.00, 0.00 },
+            new[] { 100.00, 0.00, 100.00, 0.00, 0.00 }));
+
+            localEvents.Add(new LocalEvent(
+            40,
+            "Carriage for Cinderella",
+            "This year nature promotes the growth of vegetables. Look at this pumpkin! It's the size of a carriage!",
+            1,
+            "Conduct research",
+            new[] { 1.00, 0.00, 0.10, 0.00, 0.00 },
+            new[] { 50.00, 0.00, 100.00, 0.00, 0.00 },
+            "Have a competition for the biggest pumpkin",
+            new[] { 0.00, 5.00, 0.00, 0.00, 0.00 },
+            new[] { 0.00, 100.00, 0.00, 0.00, 0.00 }));
+
+            localEvents.Add(new LocalEvent(
+            41,
+            "Bread and salt",
+            "We plan to plant a lot of grain crops. Need another mill",
+            1,
+            "Build a windmill",
+            new[] { 1.00, 0.00, 0.00, 10.00, 0.00 },
+            new[] { 50.00, 0.00, 0.00, 100.00, 0.00 },
+            "Build a water mill",
+            new[] { 1.00, 5.00, 0.00, 0.00, 0.00 },
+            new[] { 50.00, 100.00, 0.00, 0.00, 0.00 }));
+
+            localEvents.Add(new LocalEvent(
+            42,
+            "Death is just the beginning",
+            "My lord, sad news has arrived. Your friend and great warrior has died. My condolences",
+            1,
+            "Organize a magnificent funeral",
+            new[] { -3.00, 10.00, 0.00, 0.00, 1.00 },
+            new[] { 100.00, 100.00, 0.00, 0.00, 50.00 },
+            "Bury modestly",
+            new[] { 0.00, 0.00, 0.00, 0.00, 0.00 },
+            new[] { 0.00, 0.00, 0.00, 0.00, 0.00 }));
+
+            localEvents.Add(new LocalEvent(
+            43,
+            "My home is my castle",
+            "After the last attack on us, the castle walls fell into disrepair. Urgent repair required",
+            1,
+            "Fix the wall",
+            new[] { -1.00, 5.00, 0.10, -10.00, 2.00 },
+            new[] { 100.00, 50.00, 100.00, 50.00, 50.00 },
+            "Leave the wall in its current state",
+            new[] { 0.00, -3.00, 0.00, 0.00, -1.00 },
+            new[] { 0.00, 100.00, 0.00, 0.00, 100.00 }));
+
+            localEvents.Add(new LocalEvent(
+            44,
+            "The circus has left, the clowns remain",
+            "The famous circus of freaks has come to our city. You were given tickets to the best seats",
+            1,
+            "Go to a show",
+            new[] { 0.00, 0.00, 0.00, 1.00, 0.00 },
+            new[] { 0.00, 0.00, 0.00, 100.00, 0.00 },
+            "Prepare everyone to throw tomatoes",
+            new[] { 1.00, 0.00, -0.50, 0.00, 0.00 },
+            new[] { 10.00, 0.00, 100.00, 0.00, 0.00 }));
+
+            localEvents.Add(new LocalEvent(
+            45,
+            "Don't spit in the well",
+            "Peasants began to get rid of their garbage by throwing it into the river. The water has become undrinkable",
+            1,
+            "Prohibit throwing garbage into the river",
+            new[] { 0.00, -1.00, 0.10, -1.00, 0.00 },
+            new[] { 0.00, 100.00, 100.00, 100.00, 0.00 },
+            "Purify water and create a waste disposal area",
+            new[] { 0.00, 1.00, 0.00, 0.00, 0.00 },
+            new[] { 0.00, 100.00, 0.00, 0.00, 0.00 }));
+
+            localEvents.Add(new LocalEvent(
+            46,
+            "Romeo and Juliet",
+            "A conflict has arisen between the two most powerful families in the town",
+            1,
+            "Try to reconcile them",
+            new[] { 0.00, 0.00, 0.50, 0.00, 0.00 },
+            new[] { 0.00, 0.00, 100.00, 0.00, 0.00 },
+            "Support one of the families",
+            new[] { 0.00, 0.00, 0.00, -10.00, 2.00 },
+            new[] { 0.00, 0.00, 0.00, 100.00, 100.00 }));
+
+            localEvents.Add(new LocalEvent(
+            47,
+            "Templars?",
+            "A secret society has been discovered in the city. They meet every Wednesday in the castle cellars",
+            1,
+            "Execute participants",
+            new[] { 1.00, 0.00, 0.50, 0.00, 0.00 },
+            new[] { 100.00, 0.00, 100.00, 0.00, 0.00 },
+            "Spy on their activities",
+            new[] { -1.00, 5.00, 0.00, 0.00, 0.00 },
+            new[] { 100.00, 100.00, 0.00, 0.00, 0.00 }));
+
+            localEvents.Add(new LocalEvent(
+            48,
+            "No corpse, no works",
+            "The body of a man was found hanging in a noose in the city square. There is a poster on the floor: everyone is the arbiter of their own destiny",
+            1,
+            "Investigate a crime",
+            new[] { -1.00, 0.00, 0.10, 0.00, 0.00 },
+            new[] { 50.00, 0.00, 100.00, 0.00, 0.00 },
+            "Ignore",
+            new[] { 0.00, 0.00, 0.00, 0.00, 0.00 },
+            new[] { 0.00, 0.00, 0.00, 0.00, 0.00 }));
+
+            localEvents.Add(new LocalEvent(
+            49,
+            "People in the swamp",
+            "Regular rains have led to the appearance of swamps in our region",
+            1,
+            "Drain the swamp",
+            new[] { -1.00, 1.00, 0.10, 0.00, 0.00 },
+            new[] { 50.00, 50.00, 100.00, 0.00, 0.00 },
+            "Leave it to nature",
+            new[] { 0.00, 0.00, 0.10, 0.00, 0.00 },
+            new[] { 0.00, 0.00, 100.00, 0.00, 0.00 }));
+
+            localEvents.Add(new LocalEvent(
+            50,
+            "The truth is in the wine",
+            "Our vineyards became sick and almost all dried up. There won't be enough wine reserves for next year",
+            1,
+            "Treat the vineyards",
+            new[] { 0.00, 0.00, 0.10, -1.00, 0.00 },
+            new[] { 0.00, 0.00, 100.00, 100.00, 0.00 },
+            "Import wine",
+            new[] { -2.00, 1.00, 0.00, 0.00, 0.00 },
+            new[] { 100.00, 100.00, 0.00, 0.00, 0.00 }));
+
+            localEvents.Add(new LocalEvent(
+            51,
+            "Another side of the moon",
+            "For the first time in 100 years we are seeing a solar eclipse. People are terrified and waiting for the end of the world",
+            1,
+            "Reassure the population",
+            new[] { 0.00, 5.00, 0.00, 0.00, 0.00 },
+            new[] { 0.00, 100.00, 0.00, 0.00, 0.00 },
+            "Conduct scientific research",
+            new[] { 1.00, 0.00, 0.10, 0.00, 0.00 },
+            new[] { 100.00, 0.00, 50.00, 0.00, 0.00 }));
+
+            localEvents.Add(new LocalEvent(
+            52,
+            "Tremors",
+            "The earthquake destroyed several houses on the outskirts of our town. Dozens of people were left without home",
+            1,
+            "Restore destroyed buildings",
+            new[] { -5.00, 10.00, 0.00, 10.00, 0.00 },
+            new[] { 100.00, 100.00, 0.00, 100.00, 0.00 },
+            "Relocate people to safety",
+            new[] { 0.00, 0.00, 0.10, -10.00, 0.00 },
+            new[] { 0.00, 0.00, 100.00, 100.00, 0.00 }));
+
+            localEvents.Add(new LocalEvent(
+            53,
+            "Bards Festival",
+            "Tomorrow our city will host the annual bard festival. Guests will arrive from different countries",
+            1,
+            "Visit the festival",
+            new[] { 2.00, 0.00, 0.00, 0.00, 0.00 },
+            new[] { 100.00, 0.00, 0.00, 0.00, 0.00 },
+            "Perform yourself with a personal song",
+            new[] { 0.00, 2.00, 0.10, 0.00, 0.00 },
+            new[] { 0.00, 100.00, 100.00, 0.00, 0.00 }));
+
+            localEvents.Add(new LocalEvent(
+            54,
+            "After the rain on Thursday",
+            "Yesterday several fishermen went fishing and disappeared at sea. Their families are very worried",
+            1,
+            "Go in search",
+            new[] { -1.00, 0.00, 0.00, 10.00, 0.00 },
+            new[] { 100.00, 0.00, 0.00, 100.00, 0.00 },
+            "Pray for their salvation",
+            new[] { 0.00, 0.00, 0.00, 0.00, 0.00 },
+            new[] { 0.00, 0.00, 0.00, 0.00, 0.00 }));
+
             return localEvents;
         }
 
