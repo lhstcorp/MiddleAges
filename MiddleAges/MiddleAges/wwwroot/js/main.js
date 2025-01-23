10d12b4a790250b010f55f0ac0abcd3aa5071d1d
Smirnoff <68186730+lhstcorp@users.noreply.github.com>
2025-01-23 18:29:28 +0300

    feat: attribute point resetting; border land mismatches admin panel

--- MiddleAges/MiddleAges/wwwroot/js/main.js
+++ MiddleAges/MiddleAges/wwwroot/js/main.js
@@ -1,5 +1,9 @@
 ﻿$(document).ready(function () {
-    $(document).on("click", ".attrBtn", upgradeAttribute);
+    $(document).on("click", ".attrUpBtn", upgradeAttribute);
+});
+
+$(document).ready(function () {
+    $(document).on("click", ".attrDownBtn", resetAttribute);
 });
 
 function restartProduction() {
@@ -77,5 +81,49 @@ function upgradeAttribute() {
         })
         .fail(function (data) {
             alert("Unfortunately attribute wasn't upgraded");
+        });
+}
+
+function resetAttribute() {
+    var attributename = $(this).data("attributename");
+    $.ajax({
+        url: 'Main/ResetAttribute/' + attributename,
+        type: 'post',
+        datatype: 'json',
+        data: {
+            attributename: attributename
+        },
+        success: function (response) {
+            if (response == null || response == undefined || response.length == 0) {
+                return 'Error';
+            }
+            else {
+                return response;
+            }
+        },
+        error: function (response) {
+            return 'Error';
+        }
+    }).done(function (data) {
+            let obj = JSON.parse(data);
+            if (obj.Status == 'Error') {
+                alert("Attribute wasn't dropped");
+            }
+            else {
+                const attributeValue = document.getElementById(attributename);
+                attributeValue.innerText = obj.AttributeValue;
+
+                const playerMoney = document.getElementById("player-money");
+                playerMoney.innerText = obj.PlayerMoney.toFixed(2);
+
+                const availAttrPoints = document.getElementById("availAttrPoints");
+                availAttrPoints.innerText = parseInt(availAttrPoints.innerText) + 1;
+
+                const availAttrPointsMainSection = document.getElementById("availAttrPointsMainSection");
+                availAttrPointsMainSection.innerText = parseInt(availAttrPointsMainSection.innerText) + 1;
+            }
+        })
+        .fail(function (data) {
+            alert("Unfortunately attribute wasn't dropped");
         });
 }
